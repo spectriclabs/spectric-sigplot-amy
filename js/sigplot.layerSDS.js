b93e18d7ce43d3c102e0f6e7e0e043b85d74eecb
Grant Faiks <gfaiks@spectric.com>
2020-02-26 11:38:34 -0500

    Fix: updates to accomodate type 1000 files with overrides for subsize

--- js/sigplot.layerSDS.js
+++ js/sigplot.layerSDS.js
@@ -158,7 +158,12 @@
                 this.hcb.subsize = settings.subsize;
                 this.hcb.ape = settings.subsize;
                 this.hcb.size = this.hcb.dview.length / (this.hcb.spa * this.hcb.ape);
-                this.lps = Math.ceil(this.hcb.size);
+                if (hcb.file_type ===1000) {
+                    this.lps = Math.ceil(this.hcb.size/this.hcb.subsize);
+                } else {
+                    this.lps = Math.ceil(this.hcb.size);
+                }
+                    
                 var d = this.hcb.ystart + (this.hcb.ydelta * this.lps);
                 this.ymin = this.hcb.ymin || Math.min(this.hcb.ystart, d);
                 this.ymax = this.hcb.ymax || Math.max(this.hcb.ystart, d);
