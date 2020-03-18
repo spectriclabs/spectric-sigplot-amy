spectriclabs/spectric-sigplot-amy

commit 0792e7a003e614a785543b3061ecf1fe6eadc1ad

fix: reset zmin/zmax on basefile

js/sigplot.js
@@ -7185,11 +7185,15 @@
             Gx.xstart = hcb.xstart;
             Gx.xdelta = hcb.xdelta;
             Mx.origin = 1;
+            Gx.zmin = undefined;
+            Gx.zmax = undefined;
         } else {
             Gx.xstart = 0.0;
             Gx.xdelta = 1.0;
             Gx.autol = -1;
             Mx.origin = 1;
+            Gx.zmin = undefined;
+            Gx.zmax = undefined;
         }
 
         // if (!open) {

commit fc89ceaec5941d2e03c560db2cbcfb1531dc5a93

fix: regression introduced by 0792e7a

js/sigplot.js
@@ -7192,8 +7192,12 @@
             Gx.xdelta = 1.0;
             Gx.autol = -1;
             Mx.origin = 1;
-            Gx.zmin = undefined;
-            Gx.zmax = undefined;
+            if (Gx.autoz & 1) {
+                Gx.zmin = undefined;
+            }
+            if (Gx.autoz & 2) {
+                Gx.zmax = undefined;
+            }
         }
 
         // if (!open) {